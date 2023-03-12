5aa3514680069e30d62a2d7f24406a367c625fe2
ldery <derylucio@gmail.com>
2023-03-12 16:10:13 -0400

    implemented  buffer and lr scheduling -- about to test

--- trainer/scoring_model.py
+++ trainer/scoring_model.py
@@ -10,8 +10,39 @@ import pdb
 training_config = {
 	'lr' : 1e-4,
 	'bsz' : 16,
-	'nepochs' : 5
+	'nepochs' : 5,
+	'l1_weight': 0,
+	'patience': 20,
+	'patience_factor': 0.5
 }
+
+class Buffer(object):
+	def __init__(self, max_size=500):
+		self.max_size = max_size
+		self.buffer = [[], []] 
+	
+	def size(self):
+		return len(self.buffer[0])
+
+	def add_to_buffer(self, x, y):
+		self.buffer[0].extend(x)
+		self.buffer[1].extend(y)
+		if len(self.buffer[0]) > self.max_size:
+			self.buffer[0] = self.buffer[0][-self.max_size:]
+			self.buffer[1] = self.buffer[1][-self.max_size:]
+
+	def sample(self, n_samples):
+		len_ = self.size()
+		result = [[], []]
+		if len_ < n_samples:
+			return result
+		proba = np.arange(len_) + 1
+		proba = proba / proba.sum()
+		chosen = np.random.choice(len_, size=n_samples, replace=False, p=proba)
+		for i_ in chosen:
+			result[0].append(self.buffer[0][i])
+			result[1].append(self.buffer[1][i])
+		return result
 
 
 def create_tensor(shape, zero_out=1.0, requires_grad=True, is_cuda=True):
@@ -37,10 +68,20 @@ class LogisticReg(nn.Module):
 		self.optim = Adam([self.score_tensor, self.intercept], lr=training_config['lr'])
 		self.loss_fn = nn.MSELoss()
 		self.reg_fn =  nn.L1Loss() 
-		self.l1_weight = 10
+		self.l1_weight = training_config['l1_weight']
+		self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+			self.optim, factor=training_config['patience_factor'], patience=training_config['patience']
+		)
+		self.buffer = Buffer()
 
 	def update_with_info(self, run_info):
 		mask_tensors, scores = run_info
+		# get the extra samples from the buffer
+		replay_masks, replay_scores = self.buffer.sample(len(scores))
+		self.buffer.add_to_buffer(mask_tensors, scores)
+
+		mask_tensors.extend(replay_masks)
+		scores.extend(replay_scores)
 		mask_tensors = torch.stack(mask_tensors).to(self.score_tensor.device)
 		scores = torch.tensor(scores).to(self.score_tensor.device)
 		num_masks = len(mask_tensors)
@@ -50,7 +91,6 @@ class LogisticReg(nn.Module):
 			running_loss_avg = 0.0
 			n_batches = math.ceil(num_masks / training_config['bsz'])
 
-# 			print('Sum : {}, Inter {}'.format(self.score_tensor.sum().item(), self.intercept.item()))
 			for batch_id in range(n_batches):
 				start_, end_ = int(training_config['bsz'] * batch_id), int(training_config['bsz'] * (batch_id + 1))
 				xs = mask_tensors[perm[start_:end_]]
@@ -61,12 +101,12 @@ class LogisticReg(nn.Module):
 				loss = mse_loss + (self.l1_weight * l1loss)
 
 				loss.backward()
-# 				print(preds)
-# 				print('Grad max : ', torch.max(self.score_tensor.grad), 'Intercept Norm : ',  torch.norm(self.intercept.grad))
+
 				self.optim.step()
 				self.optim.zero_grad()
 				running_loss_avg += loss.item()
 			running_loss_avg /= n_batches
+			self.scheduler.step(running_loss_avg)
 			print('Epoch {} : Loss : {:.5f}'.format(epoch_, running_loss_avg))
 
 
